02a185d6acc336612c7cdd3ffac1bc6b2f217d70
Ethan Stark <20184@jhc.school.nz>
2025-03-21 09:32:52 +1300

    Added Sudo Code for tui

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #File currently for tui purposes.
+#MOC Tui
+#Please name your note
+#(In header)
+#Display Name the note
+#Display the time created
+#Typing area displays (TYPE HERE>>>) this is where the user types their note on as many line and characters as they want.
+#Command to end note /cmd:end?
+#Returns to creating, add a note ↑
