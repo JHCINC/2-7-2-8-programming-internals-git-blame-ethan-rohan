1914478f353ba69bc900094971a04895411c14a0
John <20042@jhc.school.nz>
2025-03-20 13:19:21 +1300

    modified:   modules/notes_module.py
    - Added file information.
    - Added headers that will be used to better organize the code.

---
+++ modules/notes_module.py
@@ -0,0 +1,9 @@
+"""
+Purpose: Module to manage the notes for the user
+Description: Contains functions to be able to manage the notes through main.py. 
+Author:
+"""
+
+"""
+    # ------------------------------------------------------- HEADER ------------------------------------------------------- #
+"""
