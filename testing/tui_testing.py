e8cc04d2061aad332df318bda8324308dfdd17ae
Ethan Stark <20184@jhc.school.nz>
2025-03-24 13:23:48 +1300

    Edited tui_testing/ learning curses, changed the colour of the text and also added a while loop to check for the l key to be pressed, (ALSO ADDED COMMENTS)

--- testing/tui_testing.py
+++ testing/tui_testing.py
@@ -1,9 +1,25 @@
 import curses
-def main(stdscr):
-    stdscr.clear() 
-    stdscr.addstr(0,0, "Hello, I am using Curses!")
-    stdscr.refresh()
-    stdscr.getch()
+stdscr = curses.initscr()
 
+def main(stdscr):
+    #defines color pair for text i.e. text color black, highlight color magenta
+    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_MAGENTA)
+    curses.init_pair(2,curses.COLOR_BLACK, curses.COLOR_GREEN)
+    #clears screen
+    stdscr.clear() 
+    #prints/adds the "Hello, I am using Curses!" Underline?
+    stdscr.addstr(0,0, "Hello, I am using Curses!", curses.color_pair(1))
+    #refreshes everthing
+    stdscr.refresh()
+    #This while loop checks of the "l" key to be pressed and then changes the beginniong of the text to HELLO1
+    while True:
+        key = stdscr.getch()
+        if key == ord("l"):
+            stdscr.addstr(0,0, "HELLO1", curses.color_pair(2))
+        else:
+            stdscr.addstr(0,0, "HELLO")
+    
+        
+#Runs the TUI/Closes the loop
 if __name__ == "__main__":
     curses.wrapper(main)
