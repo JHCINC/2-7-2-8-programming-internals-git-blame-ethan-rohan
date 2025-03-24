a4781a0f0747beee47638b3baa48b70d02bc6e1c
Ethan Stark <20184@jhc.school.nz>
2025-03-24 13:30:23 +1300

    Corrected/Spell Checked Comments

--- testing/tui_testing.py
+++ testing/tui_testing.py
@@ -5,13 +5,13 @@ def main(stdscr):
     #defines color pair for text i.e. text color black, highlight color magenta
     curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_MAGENTA)
     curses.init_pair(2,curses.COLOR_BLACK, curses.COLOR_GREEN)
-    #clears screen
+    #Clears screen
     stdscr.clear() 
-    #prints/adds the "Hello, I am using Curses!" Underline?
+    #prints/adds the "Hello, I am using Curses!" 
     stdscr.addstr(0,0, "Hello, I am using Curses!", curses.color_pair(1))
     #refreshes everthing
     stdscr.refresh()
-    #This while loop checks of the "l" key to be pressed and then changes the beginniong of the text to HELLO1
+    #This while loop checks of the "l" key to be pressed and then changes the beginning of the text to HELLO1
     while True:
         key = stdscr.getch()
         if key == ord("l"):
