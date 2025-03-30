400eade20db53beb238b52404b76db8f2809f674
Ethan Stark <20184@jhc.school.nz>
2025-03-31 12:59:33 +1300

    Added stdscr.getch() to main.py (without this single line the tui does not work) Created ano_Test to see if what the problem was, or if was main.py having a issue. This is how I found the stdsrc.getch() was the problem by removing line by line off of tui tesing in ano_Test.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import curses
 stdscr = curses.initscr()
 #defines main using stdscr (Standard Screen)
 def main(stdscr):
+    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_BLACK)
     #clears the terminal
     stdscr.clear()
     #adds the Hello World string to the terminal
-    stdscr.addstr(0,0, "Hello World!")
+    stdscr.addstr(0,0, "Hello World!", curses.color_pair(1))
     stdscr.refresh()
+    stdscr.getch()
 #Closes the tui loop (lets the code run)
 if __name__ == "__main__":
     curses.wrapper(main)
