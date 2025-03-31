c3fdb5c5f1bb84e96a15d85a0bb263af2e6c762e
Ethan Stark <20184@jhc.school.nz>
2025-04-01 10:49:28 +1300

    Testing dates and inputs in main.py (mainly troubleshooting)

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 import curses
-import time
+import datetime
 #defines stdscr as initial screen
 stdscr = curses.initscr()
+date = str(datetime.datetime.now())
 #defines main using stdscr (Standard Screen)
 def main(stdscr):
     curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(3,curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(4,curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(5,curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(6,curses.COLOR_RED, curses.COLOR_BLACK)
     #clears the terminal
     stdscr.clear()
     #adds the Hello World string to the terminal
-    stdscr.addstr(0,0, "Hello World!", curses.color_pair(1))
-    stdscr.refresh()
-    stdscr.getch()
+    stdscr.addstr(0,0, "Thank You for using this Note Taking App!", curses.color_pair(1))
+    stdscr.addstr(2,0, "Press the [1] to see the commands.", curses.color_pair(3)) 
     while True:
         key = stdscr.getch()
-        note = curses.echo()
-        if key == ord('+'):
+
+        if key == ord("1"):
+            stdscr.clear()
+            stdscr.addstr(0,0, "Here are the Commands:", date)
+            stdscr.addstr(2,0, "Press [+] to Add a Note", curses.color_pair(6))
+            stdscr.addstr(3,0, "Press [-] to Delete a Note", curses.color_pair(4))
+            stdscr.addstr(4,0, "Press [Shift+9] to View Previous Notes", curses.color_pair(5))
+            stdscr.addstr(6,0, "Press the [2] to go back to the main menu!", curses.color_pair(3))   
+        elif key == ord("2"):
+            stdscr.clear()
+            stdscr.addstr(0,0, "Thank You for using this Note Taking App! ", curses.color_pair(1))
+            stdscr.addstr(2,0, "Press the [1] to see the commands.", curses.color_pair(3))   
+            #Refreshes everthing
+            stdscr.refresh()
+        elif key == ord('+'):
             curses.wrapper(main)
+
+        else:
+            #If 1 is not pressed then pass (Does Nothing)
+            pass
 #Closes the tui loop (lets the code run)
 if __name__ == "__main__":
     curses.wrapper(main)
--- testing/ano_Test.py
+++ testing/ano_Test.py
@@ -1,28 +1,4 @@
-#Imports the curses module (Text User Interface MODULE)
-import curses
-#Defines stdscr = (Standard Screen)
-stdscr = curses.initscr()
+import datetime
 
-def main(stdscr):
-    #defines color pair for text i.e. text color black, highlight color magenta
-    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_BLACK)
-    curses.init_pair(2,curses.COLOR_BLACK, curses.COLOR_GREEN)
-    curses.init_pair(3,curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(4,curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(5,curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(6,curses.COLOR_RED, curses.COLOR_BLACK)
-    #Clears Terminal
-    stdscr.clear() 
-    #prints/adds the "Hello, I am using Curses!" 
-    #The two numbers represent the y and x values of the text (0,0 == y,x)
-    stdscr.addstr(0,0, "Hello, This is the testing document for this note taking app! ", curses.color_pair(1))
-    stdscr.addstr(2,0, "Press the [1] to see the commands!", curses.color_pair(3))   
-    #Refreshes everthing
-    stdscr.refresh()
-    while True:
-        #Key = get key press
-        key = stdscr.getch()
-       
-    
-if __name__ == "__main__":
-    curses.wrapper(main)
+x = str(datetime.datetime.now())
+print(x)
