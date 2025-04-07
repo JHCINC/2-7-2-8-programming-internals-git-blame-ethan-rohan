c975d111ce4c6d35e2231326e3f262fc3b852b54
Etan <158857679+Ethman4399@users.noreply.github.com>
2025-04-08 10:51:14 +1200

    Created gui_testing.py for testing and implementing code for gui

---
+++ testing/gui_testing.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+window = tk.Tk()
+window.geometry("400x400")
+window.config(bg="#000099")
+window.resizable(width=True,height=True)
+window.title('Note Taking App')
+window.state('zoomed')
+
+window.mainloop()
