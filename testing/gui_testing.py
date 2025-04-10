fb3937f3807bb61709d3d44dbe70ceb7c1a21d75
Etan <20184@jhc.school.nz>
2025-04-10 13:17:35 +1200

    Added the width for the entry box

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -9,7 +9,7 @@ window.resizable(width=True,height=True)
 window.title('Note Taking App')
 window.state('zoomed')
 
-qbox = tk.Entry(window,text="",font=("Arial",10),fg='#000099', bg="white")
+qbox = tk.Entry(window,text="",font=("Arial",10),fg='#000099', bg="white", width=200)
 test_box1 = tk.Label(window,text="This should be displaying text!",font=("Arial",20),fg="white", bg="#000099")
 
 qbox.place(y=50, x=0)
