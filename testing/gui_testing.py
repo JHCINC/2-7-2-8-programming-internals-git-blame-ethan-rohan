d87ef52f222f8a076d8a624d2d03ea150fc7f7f5
Etan <20184@jhc.school.nz>
2025-04-10 12:45:28 +1200

    test1 does this work as intended???

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from tkinter import ttk
 
 window = tk.Tk()
-window.geometry("400x400")
+window.geometry("400x401")
 window.config(bg="#000099")
 window.resizable(width=True,height=True)
 window.title('Note Taking App')
