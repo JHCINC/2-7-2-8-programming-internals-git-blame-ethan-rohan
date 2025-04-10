2488292ce7ded92c999fb184f77eac37d5b9915b
Etan <20184@jhc.school.nz>
2025-04-10 13:05:06 +1200

    Added the .place() to add the texta nd input boxes in the TK window and added the qbox to input text

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -3,10 +3,15 @@ from tkinter import messagebox
 from tkinter import ttk
 
 window = tk.Tk()
-window.geometry("400x401")
+window.geometry("400x400")
 window.config(bg="#000099")
 window.resizable(width=True,height=True)
 window.title('Note Taking App')
 window.state('zoomed')
 
+qbox = tk.Entry(window,text="",font=("Arial",10),fg='#000099', bg="white")
+test_box1 = tk.Label(window,text="This should be displaying text!",font=("Arial",20),fg="white", bg="#000099")
+
+qbox.place(y=50, x=0)
+test_box1.place(y=0,x=0)
 window.mainloop()
