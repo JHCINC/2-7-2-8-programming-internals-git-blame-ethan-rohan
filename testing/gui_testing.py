eb155052c8377d723119b796efe19880a9055bd2
Etan <20184@jhc.school.nz>
2025-04-29 10:42:10 +1200

    Added input box, title, added size/geometry and background color for naming note window.

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -5,14 +5,20 @@ from tkinter import ttk
 
 main = tk.Tk()
 box = tk.Tk()
-#note window
-title_name = tk.Entry(main, width=50)
-print('i work!!')
-title_name.pack()
+#seeing if i can make two windows at the same time
+while True:
+    main.config(bg="#000099")
+    main.title("Name Your Note")
+    main.geometry("200x150")
+    input_title = tk.Entry(main, width=20,bg="white", fg="black")
+    print('i work!!')
+    input_title.place(x=10,y=7)
+    break
 #Text Box
-box.title(title_name)
+box.title(f"{input_title}")
 box_width = tk.Text(box, width=50, height=50)
 box_width.pack()
 
 #Lets the Tk window be created
 box.mainloop()
+main.mainloop()
