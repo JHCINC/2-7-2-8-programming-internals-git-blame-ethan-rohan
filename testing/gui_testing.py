65647e6fb68d7836b2a7e178c2c5f5341abd53f5
Etan <20184@jhc.school.nz>
2025-04-29 10:50:55 +1200

    Added wait window still working on that. Going to begin including/import functions soon

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -3,19 +3,22 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 
-main = tk.Tk()
+
 box = tk.Tk()
 #seeing if i can make two windows at the same time
-while True:
-    main.config(bg="#000099")
-    main.title("Name Your Note")
-    main.geometry("200x150")
-    input_title = tk.Entry(main, width=20,bg="white", fg="black")
-    print('i work!!')
-    input_title.place(x=10,y=7)
-    break
+
+main = tk.Tk()
+main.config(bg="#000099")
+main.title("Name Your Note")
+main.geometry("200x150")
+input_title = tk.Entry(main, width=20,bg="white", fg="black")
+print('i work!!')
+input_title.place(x=10,y=7)
+
+
 #Text Box
-box.title(f"{input_title}")
+#box.title("hello")  #(f"{input_title}")
+box.wait_window()
 box_width = tk.Text(box, width=50, height=50)
 box_width.pack()
 
