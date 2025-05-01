00e007817be64f93501b3c60396ab1d02ac1fd8d
Etan <20184@jhc.school.nz>
2025-05-01 12:01:43 +1200

    Moved the text box and added the label for naming the note (Naming does not currently work)

--- testing/gui_testing.py
+++ testing/gui_testing.py
@@ -7,21 +7,20 @@ from tkinter import ttk
 box = tk.Tk()
 #seeing if i can make two windows at the same time
 
-main = tk.Tk()
-main.config(bg="#000099")
-main.title("Name Your Note")
-main.geometry("200x150")
-input_title = tk.Entry(main, width=20,bg="white", fg="black")
+box.config(bg="#000099")
+box.title("Note")
+box.geometry("200x150")
+box.state("zoomed")
+input_title = tk.Entry(box, width=20,bg="white", fg="black")
+label_title = tk.Label(box, text="Name your note here:", width=20,bg="#000099", fg="white")
 print('i work!!')
-input_title.place(x=10,y=7)
-
+input_title.place(x=250,y=25)
+label_title.place(x=225,y=0)
 
 #Text Box
-#box.title("hello")  #(f"{input_title}")
-box.wait_window()
-box_width = tk.Text(box, width=50, height=50)
-box_width.pack()
+box_width = tk.Text(box,width=140, height=50)
+box_width.place(x=250,y=85)
 
 #Lets the Tk window be created
 box.mainloop()
-main.mainloop()
+
