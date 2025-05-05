d6c7cf10cd8a9e43897d5404cdfc84458c7ae8ff
Etan <20184@jhc.school.nz>
2025-05-06 10:50:07 +1200

    Created the scroll bar, Added transparent and reopen functions to hide and show the menu bar and added buttons fro the hiding and showing.

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import tkinter as tk
 from tkinter import *
 #---------FUNCTION IMPORTS---------
 
-from src.Notes import Notes_init
+#from src.Notes import __init__ as note_ops
+#command=note_ops.create_new_note
+#---------FUNCTIONS-----------
 
+def transparent():
+    menubar.destroy()
+    contents.destroy()
+    men_collap.destroy()
+    men_open.place(x=0,y=0)
+def reopen():
+    men_collap.place(x=150, y=0)
+    menubar.pack(side=LEFT, fill=Y)
+    men_open.destroy()
+    
 #---------MENU CONFIG---------
 #This creates the main window/defines box for all gui elements.
 box = tk.Tk()
@@ -19,7 +31,7 @@ menu.add_cascade(label='File', menu=File)
 #Submenu to File tab
 #add_command adds a button under the File tab, An example of this is the New button for creating a new file
 #command=Notes.init
-File.add_command(label='New', command=Notes_init.add_note)
+File.add_command(label='New', )
 File.add_command(label='Save')
 File.add_command(label='Rename')
 File.add_command(label='Open')
@@ -32,11 +44,22 @@ Help = Menu(menu)
 menu.add_cascade(label='Help', menu=Help)
 Help.add_command(label='Source',)
 
+#-----SCROLL BAR CONFIG-------
+
+menubar = tk.Scrollbar(box)
+menubar.pack(side=LEFT, fill=Y)
+contents = Listbox(box, yscrollcommand=menubar.set)
+for cont in range(100):
+    contents.insert(END, '1')
+
+contents.pack(side=LEFT, fill=BOTH)
+menubar.config(command=contents.yview)
+
 #---------MAIN CONFIG---------
 
 #Window setup for the note app
 #Background color
-box.config(bg="#000099")
+box.config(bg="blue")
 #Title of the window that being "Note"
 box.title("Note")
 #Size of the window (Length & Width)
@@ -47,11 +70,14 @@ box.state("zoomed")
 
 #----------CODE FOR VARIABLES---------
 
+men_collap = tk.Button(box, text='-', width=1, height=1, command=transparent)
+men_open = tk.Button(box, text='+', width=1, height=1, command=reopen)
 #Text Box for type the note
 text_box = tk.Text(box,width=140, height=50, bg="white", fg="black")
 
 #Places all the variables in the box window using .place() e.g. labels, entry and text boxes 
 text_box.place(x=250,y=85)
+men_collap.place(x=150, y=0)
 
 #Lets the Tk window be created (CLOSES THE LOOP)
 box.mainloop()
